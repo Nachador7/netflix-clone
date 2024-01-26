bd61bed66b50791806931751f694e11715ed4bb6
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-26 10:34:16 -0300

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,11 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row title='Up Coming' fetchURL={requests.requestUpcoming} headers={headers} />
-      <Row title='Popular' fetchURL={requests.requestPopular} headers={headers} />
-      <Row title='Trending' fetchURL={requests.requestTrending} headers={headers} />
-      <Row title='Top Rated' fetchURL={requests.requestTopRated} headers={headers} />
-      <Row title='Horror' fetchURL={requests.requestHorror} headers={headers} />
+      <Row rowID='1' title='Up Coming' fetchURL={requests.requestUpcoming} headers={headers} />
+      <Row rowID='2' title='Popular' fetchURL={requests.requestPopular} headers={headers} />
+      <Row rowID='3' title='Trending' fetchURL={requests.requestTrending} headers={headers} />
+      <Row rowID='4' title='Top Rated' fetchURL={requests.requestTopRated} headers={headers} />
+      <Row rowID='5' title='Horror' fetchURL={requests.requestHorror} headers={headers} />
 
     </>
   )
