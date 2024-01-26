da3886cccf7248928121dd88f98afddcfd216a46
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-26 11:18:28 -0300

    changes

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <>
+    <div className='w-full h-screen'>
+        <img 
+        className='hidden sm:block absolute w-full h-full object-cover'
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/95870564-aa16-4688-8fd6-5888dcda0e79/AR-es-20240122-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="/" />
+            <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
+    </div>
+    </>
+  )
+}
+
+export default Signup
