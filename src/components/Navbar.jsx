1abdad2caa32fc2b5856fe3c8503aacb5ffebd31
Nachador7 <153395315+Nachador7@users.noreply.github.com>
2024-01-26 22:58:47 -0300

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <Link to='/'>
       <h1 
       className='text-red-600 text-4xl font-bold cursor-pointer'>
-        NETFLIX</h1>
+        NACHFLIX</h1>
         </Link>
         {user?.email ? <div>
             <Link to='/account'>
